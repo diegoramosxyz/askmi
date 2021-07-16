e99d0848a862ed9072b6817127ae1d196dc91a06
Diego Ramos <RamosDiego@pm.me>
2021-07-16 16:20:05 -0400

    added ropsten tesnet deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,29 @@
 require('@nomiclabs/hardhat-waffle')
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+require('dotenv').config()
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const { API_URL, PRIVATE_KEY } = process.env
+
+/**
+ * Hardhat Localhost network (MetaMask)
+ * Network Name: HardHat
+ * RPC URL: http://localhost:8545
+ * Chain ID: 31337
+ * Currency Symbol: ETH
+ * Block Explorer URL: <LEAVE BLANK>
+ */
+
 module.exports = {
-  solidity: '0.8.4',
+  solidity: '0.8.0',
+  defaultNetwork: 'ropsten',
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 }
